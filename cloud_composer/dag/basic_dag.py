ab259a5c58323175466ffbba6dc8abbf451aa7d7
Dapo <106677980+Dapo-code@users.noreply.github.com>
2025-10-27 13:33:57 +0000

    Create basic DAG with Bash and Python operators

---
+++ cloud_composer/dag/basic_dag.py
@@ -0,0 +1,43 @@
+import datetime
+
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+
+# pylint: disable=g-import-not-at-top
+try:
+  from airflow.providers.standard.operators.bash import BashOperator
+except ImportError:
+  from airflow.operators.bash_operator import BashOperator
+# pylint: enable=g-import-not-at-top
+
+def hello_world():
+    print("Hello World")
+    
+default_args = {
+    'start_date': datetime.datetime(2000, 1, 1),
+    'retries': 1,
+    'retry_delay': datetime.timedelta(minutes=5),
+}
+
+dag = DAG(
+    'basic_dag',
+    default_args=default_args,
+    description='Basic Dag',
+    schedule='*/10 * * * *',
+    max_active_runs=2,
+    catchup=False,
+    dagrun_timeout=datetime.timedelta(minutes=10),
+)
+
+# priority_weight has type int in Airflow DB, uses the maximum.
+bash_operator = BashOperator(
+    task_id='echo',
+    bash_command='echo test',
+    dag=dag,
+    )
+
+python_operator = PythonOperator(
+    task_id='hello_world',
+    python_callable=hello_world,
+    dag=dag,
+    )
